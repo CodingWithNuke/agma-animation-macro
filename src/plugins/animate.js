de42e7bb9afb463ab2154fac87f2cde33687c218
ronniew <rwaebers@gmail.com>
2021-10-02 20:24:16 +0200

    Updates

--- src/plugins/animate.js
+++ src/plugins/animate.js
@@ -1,13 +1,9 @@
-const { createChatContext } = require("io-scripts");
+const { createChatContext, definePlugin } = require("io-scripts");
 const { isWriting } = require("@io-scripts/utils");
 
 const store = require("../store");
 
-/**
- * @param {import('io-scripts').Script} script
- * @param {any} options
- */
-module.exports = (script, options) => {
+module.exports = definePlugin((script, options) => {
   document.addEventListener("keydown", (e) => {
     if (isWriting()) return;
 
@@ -19,4 +15,4 @@ module.exports = (script, options) => {
       chatCtx._chatElement.focus();
     }
   });
-};
+});
